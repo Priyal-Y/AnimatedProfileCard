9c3203d22a7a97e142cd491e433ccbf4c6b8b31a
Priyal Yadav <87688597+Priyal-Y@users.noreply.github.com>
2021-12-16 17:32:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let tl = gsap.timeline();
+
+console.log("New");
+
+tl.from(".card", {
+  stagger: 0.2,
+  opacity: 0,
+  x: -20,
+});
+
+tl.from(".avatar img", {
+  stagger: 0.2,
+  opacity: 0,
+  y: 20,
+});
+
+tl.from(".user-online-indicator",{
+    stagger: 0.2,
+    opacity: 0,
+    y: 20,
+})
+tl.from(".profile-name",{
+    stagger: 0.2,
+    opacity: 0,
+    y: 20,
+})
+tl.from(".profile-role",{
+    stagger: 0.2,
+    opacity: 0,
+    y: 20,
+})
+
+tl.from(".social-buttons",{
+    stagger: 0.2,
+    opacity: 0,
+})
